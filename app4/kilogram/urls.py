236f6620538922a6599391d33154f7d532940de2
siwabada <siwabada@gmail.com>
2017-04-08 17:41:49 +0900

    IndesView추가(CBV), 템플릿 파일 추가(index.html, base.html)

---
+++ app4/kilogram/urls.py
@@ -0,0 +1,11 @@
+# kilogram/urls.py
+
+from django.conf.urls import url
+from django.contrib import admin
+from . import views
+
+app_name = 'kilogram'
+
+urlpatterns = [
+ url(r'^$', views.IndexView.as_view(), name = 'index'),
+]
