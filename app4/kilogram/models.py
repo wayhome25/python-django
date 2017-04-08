0332c328c306b843eff2e80e3f6c677875135263
siwabada <siwabada@gmail.com>
2017-04-08 22:43:32 +0900

    Photo 모델 정의, admin에 Photo 모델등록, MEDIA 세팅추가

--- app4/kilogram/models.py
+++ app4/kilogram/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+def user_path(instance, filename):
+    from random import choice
+    import string
+    arr = [choice(string.ascii_letters) for _ in range(8)] #8글자 랜덤 초이스
+    pid = ''.join(arr)
+    extension = filename.split('.')[-1]
+    # monkey/abdcefgh.png
+    return '{}/{}.{}'.format(instance.owner.username, pid, extension)
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to = user_path)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    thumbnail_image = models.ImageField()
+    comment = models.CharField(max_length = 255)
+    pub_date = models.DateTimeField(auto_now_add = True)
